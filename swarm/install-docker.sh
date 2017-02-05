66b106166b8313596e4f805ce03ed9b9a746965b
David Chung <david.chung@docker.com>
2017-02-05 00:07:41 -0800

    Add instance install user
    Signed-off-by: David Chung <david.chung@docker.com>

--- swarm/install-docker.sh
+++ swarm/install-docker.sh
@@ -5,6 +5,8 @@ apt-get update -y
 apt-get upgrade -y
 wget -qO- https://get.docker.com/ | sh
 
+sudo usermod -aG docker {{ ref "/local/instance/user" }}
+
 # Tell Docker to listen on port 4243 for remote API access. This is optional.
 echo DOCKER_OPTS=\"-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock\" >> /etc/default/docker
 
