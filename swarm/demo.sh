02bcac006e23e49a29be207ada10369ff6301fb4
David Chung <david.chung@docker.com>
2017-02-05 23:08:24 -0800

    update
    Signed-off-by: David Chung <david.chung@docker.com>

--- swarm/demo.sh
+++ swarm/demo.sh
@@ -4,6 +4,9 @@
 
 mkdir -p /infrakit/plugins /infrakit/configs /infrakit/logs
 
+# For N+1 case, we use leader file and not swarm
+echo group > /infrakit/leader
+
 {{ $bashrc := cat "/home/" (ref "/compute/instance/user") "/.bashrc" | nospace }}
 {{ $dockerImage := ref "/infrakit/docker/image" }}
 {{ $dockerMounts := ref "/infrakit/docker/options/mount" }}
@@ -12,8 +15,14 @@ mkdir -p /infrakit/plugins /infrakit/configs /infrakit/logs
 
 echo "alias infrakit='docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit'" >> {{ $bashrc }}
 
-echo "Starting up infraktit"
+echo "Starting up infrakit"
 
-docker run -d --name infrakit {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit plugin start \
-       --wait --config-url {{$pluginsURL}} --exec os \
-       manager group-stateless flavor-swarm --log 5
+docker run -d --rm --name ensemble {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit plugin start \
+       --wait --config-url {{$pluginsURL}} --exec os --log 5 \
+       manager \
+       group-stateless \
+       flavor-swarm
+
+echo "Starting up instance-aws plugin"
+docker run -d --rm --name instance-aws {{$dockerMounts}} {{$dockerEnvs}} infrakit/aws:dev \
+       infrakit-instance-aws --log 5
