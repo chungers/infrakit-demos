e582a884963cf9f3f2a5546bbe9712eeb9439430
David Chung <david.chung@docker.com>
2017-02-06 16:05:29 -0800

    fixes
    Signed-off-by: David Chung <david.chung@docker.com>

--- swarm/infrakit.sh
+++ swarm/infrakit.sh
@@ -7,10 +7,8 @@ mkdir -p {{$infrakitHome}}/configs
 mkdir -p {{$infrakitHome}}/logs
 mkdir -p {{$infrakitHome}}/plugins
 
-{{ $bashrc := cat "/home/" (ref "/compute/instance/user") "/.bashrc" | nospace }}
 {{ $dockerImage := ref "/infrakit/docker/image" }}
 {{ $dockerMounts := ref "/infrakit/docker/options/mount" }}
 {{ $dockerEnvs := ref "/infrakit/docker/options/env" }}
 
-echo "alias infrakit='docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit'" >> {{ $bashrc }}
 echo "alias infrakit='docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit'" >> /root/.bashrc
