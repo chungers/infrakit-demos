419fad17698d45395fa62e0dd97fe5c12fe9b86b
David Chung <david.chung@docker.com>
2017-02-08 01:04:11 -0800

    use variables
    Signed-off-by: David Chung <david.chung@docker.com>

--- swarm/boot.sh
+++ swarm/boot.sh
@@ -11,21 +11,23 @@
 {{ $pluginsURL := cat (ref "/cluster/config/urlRoot") "/plugins.json" | nospace }}
 {{ $groupsURL := cat (ref "/cluster/config/urlRoot") "/groups.json" | nospace }}
 
+{{ $instanceImage := ref "/infrakit/instance/docker/image" }}
+{{ $instanceCmd := ref "/infrakit/instance/docker/cmd" }}
+
 echo "Starting up infrakit"
 
-docker run -d --name ensemble {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
+docker run -d --name infrakit {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
        infrakit plugin start --wait --config-url {{$pluginsURL}} --exec os --log 5 \
        manager \
        group-stateless \
        flavor-swarm
 
 echo "Starting up instance-aws plugin"
-docker run -d --name instance-aws {{$dockerMounts}} {{$dockerEnvs}} infrakit/aws:dev \
-       infrakit-instance-aws --log 5
+docker run -d --name instance-plugin {{$dockerMounts}} {{$dockerEnvs}} {{$instanceImage}} {{$instanceCmd}}
+
 
 # Need a bit of time for the leader to discover itself
 sleep 20
 
 echo "Commiting to infrakit"
-docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
-       infrakit manager commit {{$groupsURL}}
+docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit manager commit {{$groupsURL}}
