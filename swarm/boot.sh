562922aa3f45cfbc38a93e8e8e2b4593735fdbf3
David Chung <david.chung@docker.com>
2017-02-15 15:39:25 -0800

    fix
    Signed-off-by: David Chung <david.chung@docker.com>

--- swarm/boot.sh
+++ swarm/boot.sh
@@ -38,43 +38,14 @@ docker swarm init --advertise-addr {{ ref "/cluster/swarm/join/ip" }}  # starts 
 ##### Infrakit Services  #########################################################
 
 {{ if not (ref "/local/infrakit/role/worker") }}
-
 {{ include "infrakit.sh" }}
-
-{{ $dockerImage := ref "/infrakit/docker/image" }}
-{{ $dockerMounts := ref "/infrakit/docker/options/mount" }}
-{{ $dockerEnvs := ref "/infrakit/docker/options/env" }}
-{{ $pluginsURL := cat (ref "/cluster/config/urlRoot") "/plugins.json" | nospace }}
-{{ $groupsURL := cat (ref "/cluster/config/urlRoot") "/groups.json" | nospace }}
-
-{{ $instanceImage := ref "/infrakit/instance/docker/image" }}
-{{ $instanceCmd := ref "/infrakit/instance/docker/cmd" }}
-
-echo "Starting up infrakit"
-
-docker run -d --restart always --name infrakit \
-       {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
-       infrakit plugin start --wait --config-url {{$pluginsURL}} --exec os --log 5 \
-       manager \
-       group-stateless \
-       flavor-swarm
-
-echo "Starting up instance-aws plugin"
-docker run -d --restart always --name instance-plugin \
-       {{$dockerMounts}} {{$dockerEnvs}} {{$instanceImage}} {{$instanceCmd}}
-
-# Need a bit of time for the leader to discover itself
-sleep 10
-
-# Try to commit - this is idempotent but don't error out and stop the cloud init script!
-echo "Commiting to infrakit $(docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} infrakit manager commit {{$groupsURL}})"
-
 {{ end }}{{/* if running infrakit */}}
 
-
-sleep 5
-
 {{ if ref "/cluster/swarm/initialized" }}
+
+
+##### Joining Swarm  #############################################################
+sleep 5
 echo "Joining swarm"
 docker swarm join --token {{ ref "/local/docker/swarm/join/token" }} {{ ref "/local/docker/swarm/join/addr" }}
 {{ end }}
